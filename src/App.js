b75b7ccbfb07f84c2b6d38fb9d7e4e5552eecee5
Arina Leo <altynbekova.kg@gmail.com>
2022-04-28 22:45:32 -0400

    Added todo react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      inputTask: "",
+      todos: [],
+      doneTask: []
+    }
+  }
+
+  addTask = () => {
+    const { inputTask, todos: newTodos } = this.state;
+    //add task into state
+    newTodos.push(inputTask);
+    //clear inputTask
+    this.setState({ todos: newTodos, inputTask: "" })
+  }
+
+  onKeyUp = event => {
+    const { value: inputTask } = event.target;
+    // store value inside state;
+    this.setState({inputTask})
+  }
+
+  render() {
+    console.log("state", this.state)
+      return (
+    <div className="container">
+      <h1>TODO LIST</h1>
+      <div className='new-task'>
+        <input onKeyUp={this.onKeyUp} id='addTask' placeholder='Add a task' />
+        <button onClick={this.addTask} className='btn add'>Add</button>
+      </div>
+      <div className='content'> 
+      <ul>
+        {
+          this.state.todos.map(todo => {
+            return (
+              <li> <input type="checkbox"/>{todo}<span className='delete'>X</span> </li>
+            )
+          })
+        }
+      </ul>
+        <button className='clear btn'>Clear</button>
+      </div>
     </div>
   );
+  }
+
 }
 
 export default App;
