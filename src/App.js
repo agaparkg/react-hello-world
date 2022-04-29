8aba0a9f89e57255056bfe1c45530159ec7a4ff6
Arina Leo <altynbekova.kg@gmail.com>
2022-04-29 14:25:42 -0400

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,79 +1,13 @@
 import "./App.css";
 import React from "react";
+import TodoForm from "./components/TodoForm";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      inputTask: "",
-      todos: []
-    };
-  }
-
-  addTask = () => {
-    const { inputTask, todos: newTodos } = this.state;
-
-    const task = {
-      name: inputTask,
-      done: false
-    }
-    //add task into state
-    newTodos.push(task);
-    //clear inputTask
-    this.setState({ todos: newTodos, inputTask: "" });
-  };
-
-  onKeyUp = (event) => {
-    const { value: inputTask } = event.target;
-    // store value inside state;
-    this.setState({ inputTask });
-  };
-
-  delete = (ind) => {
-    const { todos } = this.state;
-    todos.splice(ind, 1)
-    this.setState({ todos })
-  }
-
-  done = (ind) => {
-    const { todos } = this.state;
-    todos[ind].done = !todos[ind].done
-    this.setState({ todos })
-  }
-
-  clearAll = () => {this.setState({ todos: []})}
-
-  render() {
-    console.log("state", this.state);
-    return (
-      <div className="container">
-        <h1>TODO LIST</h1>
-        <div className="new-task">
-          <input value={this.state.inputTask} onChange={this.onKeyUp} id="addTask" placeholder="Add a task" />
-          <button onClick={this.addTask} className="btn add">
-            Add
-          </button>
-        </div>
-        <div className="content">
-          <ul>
-            {
-            this.state.todos.map((todo, ind) => {
-              const cross = todo.done ? "cross" : "";
-              return (
-                <li key={ind} className={cross}>
-                  <input onClick={() => this.done(ind)} type="checkbox" />
-                  {todo.name}
-                  <span onClick={() => this.delete(ind)} className="delete">X</span>
-                </li>
-              );
-            })
-            }
-          </ul>
-          <button onClick={this.clearAll} className="clear btn">Clear</button>
-        </div>
-      </div>
+function App () { 
+      return (
+      <main>
+        <TodoForm />
+      </main>
     );
   }
-}
 
 export default App;
